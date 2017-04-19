bac54ee7ef4b5086e39a8485d494143229b4bc44
Nathan <asianat2332@yahoo.com>
2017-04-18 21:29:36 -0400

    Initial Commit

---
+++ NearMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NearMe
+//
+//  Created by Nathan Nguyen on 4/18/17.
+//  Copyright © 2017 Nathan Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
