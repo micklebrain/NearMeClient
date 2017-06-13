396ec424ed13f45be4757f534a9d69d842727a1d
Nathan <asianat2332@yahoo.com>
2017-06-13 14:11:40 -0400

    Included cocoa pods pulls nearby users from Dynamo DB Removing mobile hub helper stable display of nearby users according to users location

---
+++ UserIdentityAccess.swift
@@ -0,0 +1,18 @@
+//
+//  UserIdentityAccess.swift
+//  NearMe
+//
+//  Created by Nathan Nguyen on 4/20/17.
+//  Copyright © 2017 Nathan Nguyen. All rights reserved.
+//
+
+import Foundation
+
+//import AWSMobileHubHelper
+
+class UserIdentityAccess {
+    
+//    func getUserIdentity() -> String {
+//        return AWSIdentityManager.default().identityId!
+//    }
+}
